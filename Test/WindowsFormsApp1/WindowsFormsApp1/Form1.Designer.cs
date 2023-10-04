c438fc81e23f3d9a14440fc54accd973cf911d03
Thanh <nlbthanh@gmail.com>
2023-10-04 16:55:52 +0700

    Add check email

--- Test/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ Test/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -29,56 +29,31 @@ namespace WindowsFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button4 = new System.Windows.Forms.Button();
-            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.Submit = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(173, 187);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Thêm";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(154, 109);
+            this.label1.Location = new System.Drawing.Point(56, 42);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.Size = new System.Drawing.Size(61, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Nhập vào giá trị";
+            this.label1.Text = "Nhập Email";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(279, 106);
+            this.textBox1.Location = new System.Drawing.Point(134, 42);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(187, 20);
             this.textBox1.TabIndex = 2;
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(279, 187);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Tìm Kiếm";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.Button2_Click);
-            // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(382, 187);
+            this.button3.Location = new System.Drawing.Point(149, 95);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 4;
@@ -86,62 +61,38 @@ namespace WindowsFormsApp1
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.Button3_Click);
             // 
-            // dataGridView1
+            // Submit
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Column1});
-            this.dataGridView1.Location = new System.Drawing.Point(59, 282);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(463, 150);
-            this.dataGridView1.TabIndex = 5;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(59, 187);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 6;
-            this.button4.Text = "HIển Thị";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.Button4_Click);
-            // 
-            // Column1
-            // 
-            this.Column1.HeaderText = "Giá Trị";
-            this.Column1.Name = "Column1";
+            this.Submit.Location = new System.Drawing.Point(42, 95);
+            this.Submit.Name = "Submit";
+            this.Submit.Size = new System.Drawing.Size(75, 23);
+            this.Submit.TabIndex = 3;
+            this.Submit.Text = "Xác Nhận";
+            this.Submit.UseVisualStyleBackColor = true;
+            this.Submit.Click += new System.EventHandler(this.Submit_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(572, 487);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Submit);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.Button Submit;
     }
 }
 
--- Test/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ Test/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -7,12 +7,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        List_int l_int = new List_int();
         public Form1()
         {
             InitializeComponent();
@@ -23,41 +23,25 @@ namespace WindowsFormsApp1
 
         }
 
-        private void Button1_Click(object sender, EventArgs e)
-        {
-            int them = Convert.ToInt32(textBox1.Text.ToString());
-            l_int.ThemGiaTri(them);
-            MessageBox.Show("Thêm giá trị thành công.");
-        }
-
-        private void Button2_Click(object sender, EventArgs e)
-        {
-
-            int timkiem = Convert.ToInt32(textBox1.Text.ToString());
-            if (l_int.TimKiemGiaTri(timkiem))
-            {
-                MessageBox.Show("Giá trị đã được tìm thấy trong danh sách.");
-            }
-            else
-            {
-                MessageBox.Show("Giá trị không tồn tại trong danh sách.");
-            }
-
-        }
-
         private void Button3_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
 
-        private void Button4_Click(object sender, EventArgs e)
+        private void Submit_Click(object sender, EventArgs e)
         {
-            dataGridView1.DataSource = l_int;
-            DataGridViewTextBoxColumn column = new DataGridViewTextBoxColumn();
-            column.DataPropertyName = "PropertyName"; // Đặt tên thuộc tính muốn hiển thị (trong trường hợp này là "PropertyName")
-            column.HeaderText = "Column Name"; // Đặt tên cho cột trên DataGridView
-            dataGridView1.Columns.Add(column);
+            string inputEmail = textBox1.Text.Trim();
+            string emailPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+            bool isEmailValid = Regex.IsMatch(inputEmail, emailPattern);
 
+            if (isEmailValid)
+            {
+                MessageBox.Show("Email Hợp Lệ !");
+            }
+            else
+            {
+                MessageBox.Show("Email Không Hợp Lệ !");
+            }
         }
     }
 }
